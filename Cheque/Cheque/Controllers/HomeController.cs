ca46cdb7531dc61e56b96c1ac570977dab36ed57
Adam THomas <adamdthomas@gmail.com>
2017-01-06 11:16:07 -0500

    adding test to controller.

--- Cheque/Cheque/Controllers/HomeController.cs
+++ Cheque/Cheque/Controllers/HomeController.cs
@@ -35,19 +35,35 @@ namespace Cheque.Controllers
 
         public ActionResult Shop()
         {
+            string r;
+           if(Request.IsAuthenticated)
+           {
+                if (User.Identity.Name == "adamdthomas@gmail.com")
+                {
+                    string url = "http://192.168.1.18/Update";
+                    string responseText;
 
-            string url = "http://192.168.1.18/Update";
-            string responseText;
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    Stream resStream = response.GetResponseStream();
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Stream resStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(resStream);
 
-            StreamReader reader = new StreamReader(resStream);
+                    r = reader.ReadToEnd();
 
-            responseText = reader.ReadToEnd();
+                }
+                else
+                {
+                    r = "You are unauthorized to view this service.";
+                }
 
-            return View(responseText);
+            }
+            else
+            {
+                r = "You must be authenticated to view this service.";
+            }
+            return Content(r);
+
         }
 
 
@@ -56,6 +72,68 @@ namespace Cheque.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            ViewBag.Message = "message from microcontroller 1";
+
+            return View();
+        }
+
+        public ActionResult AddTime()
+        {
+            rest("http://192.168.1.18/H");
+            ViewBag.Message = "15 Minutes Added";
+
+            return View("Dashboard");
+        }
+
+        public ActionResult RemoveTime()
+        {
+            rest("http://192.168.1.18/L");
+            ViewBag.Message = "Time Reset";
+
+            return View("Dashboard");
+        }
+
+        public ActionResult Update()
+        {
+            rest("http://192.168.1.18/Update");
+            ViewBag.Message = "Data pulled from controller";
+
+            return View("Dashboard");
+        }
+
+        private string rest(string uri)
+        {
+            string r;
+            if (Request.IsAuthenticated)
+            {
+                if (User.Identity.Name == "adamdthomas@gmail.com")
+                {
+                    string url = uri;
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    Stream resStream = response.GetResponseStream();
+
+                    StreamReader reader = new StreamReader(resStream);
+
+                    r = reader.ReadToEnd();
+
+                }
+                else
+                {
+                    r = "You are unauthorized to view this service.";
+                }
+
+            }
+            else
+            {
+                r = "You must be authenticated to view this service.";
+            }
+
+            return "";
         }
     }
 }
