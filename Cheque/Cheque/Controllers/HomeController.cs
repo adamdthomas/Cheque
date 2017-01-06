9405d8196cee3002587a16ef731e9a508e2b0f65
Adam THomas <adamdthomas@gmail.com>
2017-01-06 10:08:15 -0500

    adding controls

--- Cheque/Cheque/Controllers/HomeController.cs
+++ Cheque/Cheque/Controllers/HomeController.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.IO;
 
 namespace Cheque.Controllers
 {
@@ -19,6 +27,29 @@ namespace Cheque.Controllers
 
             return View();
         }
+
+        public ActionResult Time()
+        {
+            return Json(new { time = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Shop()
+        {
+
+            string url = "http://192.168.1.18/Update";
+            string responseText;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream resStream = response.GetResponseStream();
+
+            StreamReader reader = new StreamReader(resStream);
+
+            responseText = reader.ReadToEnd();
+
+            return View(responseText);
+        }
+
 
         public ActionResult Contact()
         {
