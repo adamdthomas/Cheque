1dedac52b994db55d13a3d213331ab2082ed201e
Adam THomas <adamdthomas@gmail.com>
2017-01-17 17:05:47 -0500

    adding 4 relay control

--- Cheque/Cheque/App_Start/Utilities.cs
+++ Cheque/Cheque/App_Start/Utilities.cs
@@ -23,5 +23,39 @@ namespace HouseFly.App_Start
 
             return dicConfig;
         }
+
+        public static string MilisToClock(string milliseconds)
+        {
+            int iMillis = int.Parse(milliseconds);
+            int hourLeft = iMillis / 3600000;
+            int minLeft = iMillis / 60000;
+            int secLeft = iMillis / 1000;
+            secLeft = secLeft - (minLeft * 60);
+            minLeft = minLeft - (hourLeft * 60);
+            string sSecLeft = secLeft.ToString();
+            string sMinLeft = minLeft.ToString();
+            string sHourLeft = hourLeft.ToString();
+
+            if (sSecLeft.Length < 2)
+            {
+                sSecLeft = "0" + sSecLeft;
+            }
+
+            if (sMinLeft.Length < 2)
+            {
+                sMinLeft = "0" + sMinLeft;
+            }
+
+            if (sHourLeft.Length < 2)
+            {
+                sHourLeft = "0" + sHourLeft;
+            }
+
+            string tl = sHourLeft + ":" + sMinLeft + ":" + sSecLeft;
+
+          
+
+            return tl;
+        }
     }
 }
--- Cheque/Cheque/Controllers/HomeController.cs
+++ Cheque/Cheque/Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace HouseFly.Controllers
     public class HomeController : Controller
     {
         private string garageBenchURL = @"http://192.168.1.18/";
+        //private string garageBenchURL = @"http://10.238.243.174/";
         public ActionResult Index()
         {
             return View();
@@ -35,6 +36,7 @@ namespace HouseFly.Controllers
             return View();
         }
 
+        [Authorize(Users ="adamdthomas@gmail.com")]
         [HttpPost]
         public ActionResult Video(string str)
         {
@@ -56,15 +58,12 @@ namespace HouseFly.Controllers
             return RedirectToAction("Video");
         }
 
-
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
+  
         public ActionResult Time()
         {
             return Json(new { time = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}, JsonRequestBehavior.AllowGet);
@@ -84,15 +83,42 @@ namespace HouseFly.Controllers
         }
 
         [HttpPost]
-        public ActionResult Dashboard(string min)
+        public ActionResult Dashboard(string min, string relay)
         {
-            rest(garageBenchURL + "addmin!" + min + "!", "Garage");
-            ViewBag.Message = min + " Minutes Added";
-            return RedirectToAction("Dashboard");
+            try
+            {
+                min = (int.Parse(min) * 60000).ToString();
+                rest(garageBenchURL + "time!" + min + "!" + relay + "!", "Garage");
+                ViewBag.Message = min + " Minutes Added";
+               
+            }
+            catch (Exception)
+            {
 
+            }
+
+            return RedirectToAction("Dashboard");
         }
 
-        public ActionResult AddTime()
+
+        public ActionResult HandleTime(string relay, string time)
+        {
+            try
+            {
+                time = (int.Parse(time) * 60000).ToString();
+                rest(garageBenchURL + "time!" + time + "!" + relay + "!", "Garage");
+                ViewBag.Message = time + " Minutes Added";
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return RedirectToAction("Dashboard");
+        }
+
+        public ActionResult AddTime(string rnum)
         {
             rest(garageBenchURL + "H", "Garage");
             ViewBag.Message = "15 Minutes Added";
@@ -168,7 +194,13 @@ namespace HouseFly.Controllers
                         ViewBag.Hour = JD["HourRemaining"];
                         ViewBag.Min = JD["MinutesRemaining"];
                         ViewBag.Sec = JD["SecondsRemaining"];
-                        ViewBag.RelayOne = JD["RelayOneStatus"];
+
+                        ViewBag.r1t = Utilities.MilisToClock(JD["r1t"]);
+                        ViewBag.r2t = Utilities.MilisToClock(JD["r2t"]);
+                        ViewBag.r3t = Utilities.MilisToClock(JD["r3t"]);
+                        ViewBag.r4t = Utilities.MilisToClock(JD["r4t"]);
+
+
 
                         break;
                     default:
